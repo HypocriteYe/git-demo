1dbc898db07e1ab2de19f69bf3d202bdad246b8a
93475 <934752370@qq.com>
2022-04-24 15:39:47 +0800

    测试SSH

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("创建分支");
         System.out.println("在线更新");
+        System.out.println("SSH上传");
     }
 }
